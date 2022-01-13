a3061e60ff647ab3f8c071337abead3ba3fb5fbc
jseidel <js@deltacity.net>
2022-01-13 22:28:34 +0100

    styles and new cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -26,7 +26,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was created successfully' }
+        format.html { redirect_to store_index_url, notice: 'Added item to cart' }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -53,7 +53,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to @line_item.cart, notice: 'Line item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -67,6 +67,6 @@ class LineItemsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def line_item_params
-    params.require(:line_item).permit(:product_id, :cart_id)
+    params.require(:line_item).permit(:product_id)
   end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -17,4 +17,8 @@ class Cart < ApplicationRecord
     # return the value of current_item
     current_item
   end
+
+  def total_price
+    line_items.to_a.sum(&:total_price)
+  end
 end
