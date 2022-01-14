9ebb24e6100ba08de694a20f7c31cae990b94242
Justin Seidel <js@deltacity.net>
2022-01-14 12:35:06 +0100

    nice

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -6,7 +6,6 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select 'nav.side_nav a', minimum: 4
     assert_select 'main ul.catalog li', 3
-    assert_select 'h2', 'Your Cart'
     assert_select '.price', /[,\d]+\.\d\d €/
   end
 end
