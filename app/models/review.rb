9c0a601f3da6ce15faff2532aafb6e177707c133
Justin Seidel <js@deltacity.net>
2022-01-26 09:24:24 +0100

    Reviews work properly

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,8 @@ class Review < ApplicationRecord
   belongs_to :product
   belongs_to :user, optional: true
   validates :body, presence: true
+
+  def userinfo
+    User.find_by id: user_id
+  end
 end
